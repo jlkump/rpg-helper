421fb6706cd3039a62f2b7e27dd5363d34c2e1d4
Jonathan Kump <landon2002@gmail.com>
2024-06-07 17:06:08 -0600

    Figured out how to handle multiple mutable references for web server. Good to go for development

--- server/src/api/handlers/login.rs
+++ server/src/api/handlers/login.rs
@@ -1,9 +1,17 @@
 use actix_web::{get, web, Error, HttpResponse};
 
-use crate::database::user::{UserDB, UserLoginSchema};
+use crate::database::user::{LoginResponse, UserDB, UserLoginSchema};
 
 #[get("/api/login")]
-async fn login(login: web::Json<UserLoginSchema>) -> Result<HttpResponse, Error> {
-    // TODO: Where to put DB since we have async?
-    todo!()
+async fn login(login: web::Json<UserLoginSchema>, user_db: web::Data<UserDB>) -> Result<HttpResponse, Error> {
+    // When the user login is successful,
+    // the user has secure login. Otherwise, not logged in.
+    match user_db.login_user(login.into_inner()) {
+        LoginResponse::Success(user) => {
+            // User logged in successfully
+            todo!()
+        }, 
+        LoginResponse::UnknownUsername => todo!(),
+        LoginResponse::WrongPassword => todo!(), // Handle limited number of tries
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,8 @@
 use actix_cors::Cors;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use api::routes;
 use config::Config;
+use database::user::UserDB;
 
 mod config;
 mod database;
@@ -10,20 +11,28 @@ mod api;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = Config::from_file("Config.toml").unwrap();
+    let user_db = web::Data::new(UserDB::open(&config));
 
     println!("Starting server at http://{}:{}/", config.server.host, config.server.port);
     HttpServer::new(move || {
         let cors = Cors::default()
-        .allow_any_origin()
-        .allow_any_method()
-        .allow_any_header()
-        .max_age(3600);
+            .allow_any_origin() // TODO: Change later to allow only certain Origins, Methods, and Headers
+            .allow_any_method()
+            .allow_any_header()
+            .max_age(3600);
 
         App::new()
             .wrap(cors)
+            .app_data(user_db.clone()) // Passing user DB to worker threads
             .configure(routes::initialize)
     })
     .bind((config.server.host, config.server.port))?
     .run()
     .await
+}
+
+// How to get the UserDB database info
+async fn index(data: web::Data<UserDB>) -> String {
+    todo!();
+    // let mut counter = data.get_data(0);
 }
