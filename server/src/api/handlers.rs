2ca78c0b5b9e10c521bf8b1eaa0088615433b560
Jonathan Kump <landon2002@gmail.com>
2024-06-08 13:38:54 -0600

    Added health-checker to check api server integrity

--- server/src/api/handlers.rs
+++ server/src/api/handlers.rs
@@ -1,6 +1,14 @@
-use actix_web::web;
+use actix_web::{get, web, HttpResponse, Responder};
+use serde_json::json;
 
 mod user;
+
+#[get("/healthchecker")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "Rpg-helper website made in Rust.";
+
+    HttpResponse::Ok().json(json!({"status": "success", "message": MESSAGE}))
+}
 
 pub fn setup_routes(cfg: &mut web::ServiceConfig) -> &mut web::ServiceConfig {
     let scope = web::scope("/api")
