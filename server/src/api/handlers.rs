38fdb03aed4f62f7d397fc73689ec4566714a5d1
Jonathan Kump <landon2002@gmail.com>
2024-06-08 11:56:48 -0600

    registration using jwt auth

--- server/src/api/handlers.rs
+++ server/src/api/handlers.rs
@@ -1,6 +1,7 @@
 use actix_web::web;
 
 mod login;
+mod register;
 
 pub fn setup_routes(cfg: &mut web::ServiceConfig) -> &mut web::ServiceConfig {
     todo!()
---
+++ server/src/api/handlers/register.rs
@@ -0,0 +1,47 @@
+use actix_web::{post, web, Error, HttpResponse, Responder};
+
+use crate::database::user::{UserDB, UserRegistrationSchema};
+
+
+#[post("/auth/register")]
+async fn register_user_handler(
+    body: web::Json<UserRegistrationSchema>,
+    db: web::Data<UserDB>,
+) -> impl Responder {
+    let exists = db.has_email(body.email);
+
+    if exists {
+        return HttpResponse::Conflict().json(
+            serde_json::json!({"status": "fail","message": "User with that email already exists"}),
+        );
+    }
+
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = Argon2::default()
+        .hash_password(body.password.as_bytes(), &salt)
+        .expect("Error while hashing password")
+        .to_string();
+    let query_result = sqlx::query_as!(
+        User,
+        "INSERT INTO users (name,email,password) VALUES ($1, $2, $3) RETURNING *",
+        body.name.to_string(),
+        body.email.to_string().to_lowercase(),
+        hashed_password
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match query_result {
+        Ok(user) => {
+            let user_response = serde_json::json!({"status": "success","data": serde_json::json!({
+                "user": filter_user_record(&user)
+            })});
+
+            return HttpResponse::Ok().json(user_response);
+        }
+        Err(e) => {
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"status": "error","message": format!("{:?}", e)}));
+        }
+    }
+}
