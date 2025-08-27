97d73ca1acc4ca6c7c8320ba0b494d9992a740bc
jlkump1 <landon2002@gmail.com>
2025-08-26 22:15:08 -0500

    Minor style adjustment test

--- client/src/app/gui/pages/editor/ruleset.rs
+++ client/src/app/gui/pages/editor/ruleset.rs
@@ -18,16 +18,21 @@ pub fn ruleset_editor() -> Html
                 <form>
                     <h3>{"Date"}</h3>
                     <hr/>
-                    <label for="test">{"Ordering Equation"}<i class="fa-solid fa-circle-question"></i></label>
-                    <input class="" type="text" name="test" placeholder="(rhs.Year - lhs.Year) * 365"/>
-                    <label for="test">{"Required Values"}<i class="fa-solid fa-circle-question"></i></label>
-                    <input class="" type="text" name="test" placeholder="Year"/>
+                    <label>{"Ordering Equation"}<i class="fa-solid fa-circle-question"></i></label>
+                    <input class="" type="text" placeholder="(rhs.Year - lhs.Year) * 365"/>
+                    <label>{"Required Values"}<i class="fa-solid fa-circle-question"></i></label>
+                    <input class="" type="text" placeholder="Year"/>
                 </form>
             </div>
         
             <div style="background-color: var(--paper-25); padding: 10px; margin: 4px; max-width: 60vw; box-shadow: 0px 4px 4px var(--drop-shadow);">
                 <h1>{"Header 1"}</h1>
                 <hr/>
+                <label>{"Equation"}<i class="fa-solid fa-circle-question"></i></label>
+                <input class="" type="text" placeholder="Year"/>
+
+                <label>{"Equation"}<i class="fa-solid fa-circle-question"></i></label>
+                <input class="temp-in" style="color: var(--text-primary); background-color: var(--primary);" type="text" placeholder="Alt Year"/>
                 <p>
                     {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
                 </p>
